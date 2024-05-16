9862c48112d1ed8cc2df95978ab899e133df1a0f
suyama_naoki <naoki101130@icloud.com>
2024-05-16 16:24:11 +0900

    Fix

--- Sources/TestPackage/TestPackage.swift
+++ Sources/TestPackage/TestPackage.swift
@@ -1,5 +1,13 @@
 open class Dog{
     public var name: String = "pochi"
+    
+    public init(name: String){
+        self.name = name
+    }
+}
+
+public func create_dog(name: String) -> Dog {
+    return Dog(name: name)
 }
 
 public func get_2dogs_name(_ a: Dog, _ b: Dog){
